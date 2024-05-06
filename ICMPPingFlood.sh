c681f8fe4e98f905345f90fa1c28a1b5fd48f134
TRXe8 <169096534+TRXe8@users.noreply.github.com>
2024-05-06 16:27:23 -0500

    Update ICMPPingFlood.sh
    Added IP of VM + minor edits

--- ICMPPingFlood.sh
+++ ICMPPingFlood.sh
@@ -1,12 +1,12 @@
 !/usr/bin/bash
 
-ip.addr="" #Change Target IP as required
+ip.addr="192.168.63.132" #Change Target IP as required
 max_pings=100
 counter=0
 
 while [ $count -lt $max_pings ]
 do
-    if [ping -c1 $ip_addr >/dev/null];
+    if [ping -c 1 $ip_addr >/dev/null];
     then
         echo "Ping Sent"
     else
