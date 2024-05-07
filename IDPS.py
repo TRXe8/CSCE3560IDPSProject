d58aa01290754ba28747c5dc8943db009528f338
TRXe8 <169096534+TRXe8@users.noreply.github.com>
2024-05-07 07:59:44 -0500

    Update IDPS.py
    Added ufw rule as part of the mitigation, as well as more verbosity to the code

--- IDPS.py
+++ IDPS.py
@@ -11,15 +11,19 @@ def icmpMit():
                 ICMP_IP_ADDR[src_ip] = 1
             else:
                 ICMP_IP_ADDR[src_ip] += 1
-                if ICMP_IP_ADDR[src_ip] > 5:
-                    #if ICMP_IP_ADDR[src_ip] = 6:
-                    
-                    #    print(f"Blocking ICMP Request from {src_ip}")
-                    #    os.system("sudo ufw deny icmp from src_ip")
-                    #else:
-                        print (f"ICMP request from {src_ip} blocked")
-                        return
-        send(packet)
+                if ICMP_IP_ADDR[src_ip] == 6:
+                    print(f"Blocking ICMP Requests from {src_ip}")
+                    result = os.system(f"sudo ufw deny proto icmp from {src_ip}")
+                    blocked = 0
+                    if result == 0:
+                        print(f"Successfully blocked {src_ip}")
+                    else
+                        print(f"Error blocking {src_ip}")
+                elif ICMP_IP_ADDR[src_ip] > 6:
+                    blocked += 1
+                    if blocked % 10 == 0:
+                        print(f"ICMP requests from {src_ip} have been blocked {blocked} times")
+
     sniff(filter="icmp", prn=icmp_mitigation)
 
 def synMit():
@@ -32,14 +36,19 @@ def synMit():
                 SYN_IP_ADDR[src_ip] = 1
             else:
                 SYN_IP_ADDR[src_ip] += 1
-                if SYN_IP_ADDR[src_ip] > 5:
-                    #if SYN_IP_ADDR[src_ip] = 6:
-                        #print (f"Blocking ICMP Request from {src_ip}")
-                        #os.system("sudo ufw deny 80/tcp from src_ip")
-                    #else:
-                    print (f"SYN Request from {src_ip} blocked")
-                    return
-        send(packet)
+                if SYN_IP_ADDR[src_ip] == 6:
+                    print(f"Blocking SYN requests from {src_ip}")
+                    result = os.system(f"sudo ufw deny proto 80/tcp from {src_ip}")
+                    blocked = 0
+                    if result == 0:
+                        print(f"Successfully blocked {src_ip}")
+                    else
+                    print(f"Error blocking {src_ip}")
+                elif SYN_IP_ADDR[src_ip] > 6:
+                    blocked += 1
+                    if blocked % 10 == 0:
+                        print(f"SYN Requests from {src_ip} have been blocked {blocked} times")
+
     sniff(filter="tcp", prn = syn_mitigation)
             
 p1 = multiprocessing.Process(target=icmpMit)
