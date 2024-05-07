4120605ed108a4a5cd2599ffd697ea591b4b45ad
TRXe8 <169096534+TRXe8@users.noreply.github.com>
2024-05-07 06:43:42 -0500

    Update IDPS.py
    Added concept for a ufw rule being written when threshold is reached

--- IDPS.py
+++ IDPS.py
@@ -12,8 +12,13 @@ def icmpMit():
             else:
                 ICMP_IP_ADDR[src_ip] += 1
                 if ICMP_IP_ADDR[src_ip] > 5:
-                    print ("Blocking ICMP request from {src_ip}")
-                    return
+                    #if ICMP_IP_ADDR[src_ip] = 6:
+                    
+                    #print(f"Blocking ICMP Request from {src_ip}")
+                    #    os.system("sudo ufw deny 80/tcp from src_ip")
+                    #else:
+                        print (f"ICMP request from {src_ip} blocked")
+                        return
         send(packet)
     sniff(filter="icmp", prn=icmp_mitigation)  
 p1 = multiprocessing.Process(target=icmpMit)
